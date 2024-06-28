5dacddde153be83d367c53a4c1d17483d657176c
lchaconc <luis.chacon.campos@gmail.com>
2024-06-28 14:47:09 -0600

    cambio de punto de entrada. Se elimina index.js

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import {createRoot} from "react-dom/client"
-import Home from "./components/Home.jsx";
-
-
-const root = createRoot(document.getElementById("root") );
-root.render(
-    <Home />
-)
